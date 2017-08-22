08021461447b41071c82869c8f3b016d3f28ad37
tongy <toy.1.tong@nokia-sbell.com>
2017-08-22 22:08:35 +0800

    replace all tabs to spaces

--- CreationalDesignPatterns/singleton/Singleton.cpp
+++ CreationalDesignPatterns/singleton/Singleton.cpp
@@ -10,24 +10,24 @@ template <class T>
 class Singleton
 {
 protected:
-	Singleton(){}
+    Singleton(){}
 public:
-	static T* instance()//返回某类型的单例对象
-	{
-		if(m_pInstance == NULL)//这里进行了两遍检查
-		{
-			pthread_mutex_lock(&mutex);//上锁
-			if(m_pInstance == NULL)//保证只能执行一次
-			{
-				T* temp = new T;//构造一个新的对象
-				m_pInstance = temp;
-			}
-			pthread_mutex_unlock(&mutex);//解锁
-		}
-		
-		return m_pInstance;//返回该类型的单例对象
-	}
-	
+    static T* instance()//返回某类型的单例对象
+    {
+        if(m_pInstance == NULL)//这里进行了两遍检查
+        {
+            pthread_mutex_lock(&mutex);//上锁
+            if(m_pInstance == NULL)//保证只能执行一次
+            {
+                T* temp = new T;//构造一个新的对象
+                m_pInstance = temp;
+            }
+            pthread_mutex_unlock(&mutex);//解锁
+        }
+        
+        return m_pInstance;//返回该类型的单例对象
+    }
+    
 private:
     static T* m_pInstance;
     static pthread_mutex_t mutex;//需要互斥锁
@@ -42,39 +42,39 @@ pthread_mutex_t Singleton<T>::mutex;
 class Test:public Singleton<Test>   
 {
 public:
-	Test()
-	{
-		cout << "This will be printed only once!\n" << endl;
-	}
+    Test()
+    {
+        cout << "This will be printed only once!\n" << endl;
+    }
 };
 
 
 void* thread1(void*)//第一组线程的线程函数
 {
-	cout << "thread1 is running\n" << endl;
-	Test* test1 = Test::instance();//调用该函数，但是Test对象只会创建一次
+    cout << "thread1 is running\n" << endl;
+    Test* test1 = Test::instance();//调用该函数，但是Test对象只会创建一次
 }
 
  void* thread2(void *)//第二组线程的线程函数
 {
-	cout << "thread2 is running\n" << endl;
-	Test* test2 = Test::instance();//调用该函数，但是Test对象只会创建一次
+    cout << "thread2 is running\n" << endl;
+    Test* test2 = Test::instance();//调用该函数，但是Test对象只会创建一次
 }
 int main()
 {
-	const unsigned int thread_num = 5;
-	pthread_t thread_id[thread_num];
-	
-	for(unsigned int i = 0; i<thread_num;i++)//创建1组5个线程
-	{
-		pthread_create(&thread_id[i], NULL, thread1, NULL);
-	}
-	
-	for(unsigned int i = 0; i<thread_num;i++)//创建另一组的五个线程
-	{
-		pthread_create(&thread_id[i], NULL, thread2, NULL);
-	}
-	
-	sleep(1);//等待线程结束
-	return 0;
+    const unsigned int thread_num = 5;
+    pthread_t thread_id[thread_num];
+    
+    for(unsigned int i = 0; i<thread_num;i++)//创建1组5个线程
+    {
+        pthread_create(&thread_id[i], NULL, thread1, NULL);
+    }
+    
+    for(unsigned int i = 0; i<thread_num;i++)//创建另一组的五个线程
+    {
+        pthread_create(&thread_id[i], NULL, thread2, NULL);
+    }
+    
+    sleep(1);//等待线程结束
+    return 0;
 } 
--- CreationalDesignPatterns/singleton/Singleton_2.cpp
+++ CreationalDesignPatterns/singleton/Singleton_2.cpp
@@ -7,15 +7,15 @@
 class singleton{
 protected:
     singleton()
-	{
+    {
         pthread_mutex_init(&mutex, nullptr);
-	}
+    }
 
 private:
     static singleton* p;
 
 public:
-	static pthread_mutex_t mutex;
+    static pthread_mutex_t mutex;
     static singleton* instance();
 };
 
@@ -24,47 +24,47 @@ singleton* singleton::p = nullptr;
 singleton* singleton::instance()
 {
     if(p == nullptr)
-	{
-		pthread_mutex_lock(&mutex);
-		if(p == nullptr)
-		{
-			p = new singleton();
-			printf("I will print only once\n");
-		}
-		pthread_mutex_unlock(&mutex);
-	}
+    {
+        pthread_mutex_lock(&mutex);
+        if(p == nullptr)
+        {
+            p = new singleton();
+            printf("I will print only once\n");
+        }
+        pthread_mutex_unlock(&mutex);
+    }
 
     return p;
 }
 
 void* thread1(void*)//第一组线程的线程函数
 {
-	std::cout << "thread1 is running\n" << std::endl;
-	singleton::instance();
+    std::cout << "thread1 is running\n" << std::endl;
+    singleton::instance();
 }
 
  void* thread2(void *)//第二组线程的线程函数
 {
-	std::cout << "thread2 is running\n" << std::endl;
-	singleton::instance();
+    std::cout << "thread2 is running\n" << std::endl;
+    singleton::instance();
 }
 
 int main(void)
 {
-	const unsigned int thread_num = 5;
-	pthread_t thread_id[thread_num];
-	
-	for(unsigned int i = 0; i<thread_num;i++)//创建1组5个线程
-	{
-		pthread_create(&thread_id[i], NULL, thread1, NULL);
-	}
-	
-	for(unsigned int i = 0; i<thread_num;i++)//创建另一组的五个线程
-	{
-		pthread_create(&thread_id[i], NULL, thread2, NULL);
-	}
-	
-	sleep(1);	// 等待线程结束
-	
-	return 0;
+    const unsigned int thread_num = 5;
+    pthread_t thread_id[thread_num];
+    
+    for(unsigned int i = 0; i<thread_num;i++)//创建1组5个线程
+    {
+        pthread_create(&thread_id[i], NULL, thread1, NULL);
+    }
+    
+    for(unsigned int i = 0; i<thread_num;i++)//创建另一组的五个线程
+    {
+        pthread_create(&thread_id[i], NULL, thread2, NULL);
+    }
+    
+    sleep(1);   // 等待线程结束
+    
+    return 0;
 }
